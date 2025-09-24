0b9a60a7dccee18eb6ed4eb5faa0e9456cd39a18
Катерина <katerinaegorova1489@mail.ru>
2025-09-24 13:17:30 +0300

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,7 @@
+globalVar = 1
+# кв 35
+def printGlobal():
+    print(globalVar)
+# кв 36
+
+
---
+++ lesson1_stack.py
@@ -0,0 +1,28 @@
+def funcA():
+    print("Начали выполнять А")
+    funcB()
+    print("Закончили выполнять А")
+
+def funcB():
+    print("Начали выполнять B")
+    funcC()
+    print("Закончили выполнять B")
+
+def funcC():
+    print("Начали выполнять C")
+    funcD()
+    print("Закончили выполнять C")
+
+def funcD():
+    print("Начали выполнять D")
+    
+    print("Закончили выполнять D")
+
+
+#funcA()
+
+def endless():
+    print("endless")
+    endless()
+
+endless()
